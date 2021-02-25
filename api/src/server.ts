bf750c21a7eb65bc6b24711e13cd1fb7273c9607
Pablo <pablowoina2205@gmail.com>
2021-02-25 16:39:52 -0300

    Aula 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database'
+import { router } from './routes';
 
 const app = express();
-
-app.get('/', (request, response) => {
-  return response.json({message : "Hello World - NLW #4"})
-})
-
-app.post('/', (request, response) => {
-  // recebeu dados para salvar
-  return response.json({message : "os dados foram salvos com sucesso!"})
-
-})
-
-
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => {console.log('Server is running!')})
 
